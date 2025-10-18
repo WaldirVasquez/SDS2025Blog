a9a44a1fd06dc08f2e2906a602c81b669470d9f7
Waldir21 <vr19030@ues.edu.sv>
2025-10-17 21:01:41 -0600

    dia 5 vista terminada con fotos descripciones de las actividades y videos

--- blog_MVC/app/views/dias/dia5.php
+++ blog_MVC/app/views/dias/dia5.php
@@ -25,57 +25,86 @@
   <section class="section-card" data-anim>
     <h2 class="section-title">9:00 AM · BINGO</h2>
     <div class="meta">Auditorio 1</div>
-    <p class="desc">Breve mensaje de apertura, presentación del programa general y palabras de bienvenida a cargo del comité organizador. Se invita a los asistentes a compartir en redes con el hashtag <strong>#SDS25</strong>.</p>
+    <p class="desc">Ultimo dia de la semana de sistemas 2025 y termino con un dia lleno de risas, emociones, iniciando el dia 5 con el juego de bingo
+                    donde muchos estudiantes y docentes participaron, en este juego tuvo mucha suerte la Ingeniera Vanesa ya que gano tres rondas.<br>
+                    Para ver las fotos completas dar clic en ellas <strong>#SDS25</strong>.</p>
     <div class="grid grid--5" style="margin-top:16px">
-      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760588832/sds25_qneaz5.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760588832/sds25_qneaz5.jpg" alt="Inauguración – Foto 1" loading="lazy">
-        <figcaption class="overlay">SDS2025</figcaption>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760751539/bingoo_qi6pbc.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760751539/bingoo_qi6pbc.jpg" alt="Bingo – Foto 1" loading="lazy">
+        <figcaption class="overlay">BINGO</figcaption>
       </figure>
-      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760589209/presidenta_p57l0k.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760589209/presidenta_p57l0k.jpg" alt="Inauguración – Foto 2" loading="lazy">
-        <figcaption class="overlay">Presidenta Karla Orellana danado paralabras de bienvenida</figcaption>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760751654/bingo1_xzb1y6.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760751654/bingo1_xzb1y6.jpg" alt="Bingo – Foto 2" loading="lazy">
+        <figcaption class="overlay">BINGO</figcaption>
       </figure>
-      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760591304/doctora_vwcgrz.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760591304/doctora_vwcgrz.jpg" alt="Inauguración – Foto 3" loading="lazy">
-        <figcaption class="overlay">Palabras de la Doctora Azucena Retana Vicedecana</figcaption>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760751646/bingo2_dw23hf.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760751646/bingo2_dw23hf.jpg" alt="Bingo – Foto 3" loading="lazy">
+        <figcaption class="overlay">En la primera ronda del Bingo</figcaption>
       </figure>
-      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760590669/Diego_y3sgb7.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760590669/Diego_y3sgb7.jpg" alt="Inauguración – Foto 4" loading="lazy">
-        <figcaption class="overlay">El Ing. Diego Herrera dando palabras de motivacion</figcaption>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760751539/bingo4_hxyihu.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760751539/bingo4_hxyihu.jpg" alt="Bingo – Foto 4" loading="lazy">
+        <figcaption class="overlay">Un comapañero jugando bingo</figcaption>
       </figure>
-      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760666485/foto_mia_1_lqcooe.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760666485/foto_mia_1_lqcooe.jpg" alt="Inauguración – Foto 5" loading="lazy">
-        <figcaption class="overlay">Mi participa en la asistencia de las ponencias</figcaption>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760751537/bingo3_klj2yb.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760751537/bingo3_klj2yb.jpg" alt="Bingo – Foto 5" loading="lazy">
+        <figcaption class="overlay">Estuve a punto de ganar, en una de las rondas de los audifonos</figcaption>
       </figure>
     </div>
 
     <div class="grid grid--2" style="margin-top:16px">
-      <div class="video-wrap"><video src="https://res.cloudinary.com/dqubpavb8/video/upload/v1760591700/Karla_mtbeh3.mp4" controls playsinline></video></div>
-      <div class="video-wrap"><video src="https://tu-servidor.com/media/ina_video_1.mp4" controls playsinline></video></div>
+      <div class="video-wrap"><video src="https://res.cloudinary.com/dqubpavb8/video/upload/v1760752757/20251017_094733_nvlhd8.mp4" controls playsinline></video></div>
+      <div class="video-wrap"><video src="https://res.cloudinary.com/dqubpavb8/video/upload/v1760752576/20251017_104204_ga9goi.mp4" controls playsinline></video></div>
+    </div>
+  </section>
+
+  <!-- entrega de diplomas -->
+  <section class="section-card" data-anim>
+    <h2 class="section-title">Entrega de diplomas y premios al mejor stand y ala votacion de facebook</h2>
+    <div class="meta"> Auditorio 1</div>
+    <p class="desc">Se entregaron diplomas de reconocimineto a los estudiantes que presentaron sus proyectos en la feria de logros<br>
+                    Asi como la premiacion a los mejores stands y al proyecto mas votados en facebook.<br>
+                    Para ver las fotos completas dar clic en ellas <strong>#SDS25</strong>.</p>
+    <div class="grid grid--5" style="margin-top:16px">
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760753719/20251017_121811_bt27vu.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760753719/20251017_121811_bt27vu.jpg" alt="Entrega de diplomas – Foto 1" loading="lazy">
+        <figcaption class="overlay">Inicio de entrega de diplomas</figcaption>
+      </figure>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760753518/dimopla3_ojrcqw.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760753518/dimopla3_ojrcqw.jpg" alt="Entrega de diplomas – Foto 2" loading="lazy">
+        <figcaption class="overlay">Estudiantes con los diplomas</figcaption>
+      </figure>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760755307/diploma01_ndlsom.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760755307/diploma01_ndlsom.jpg" alt="Diplomas – Foto 3" loading="lazy">
+        <figcaption class="overlay">Mi persona reciviendo el diploma, entregado por la Ing. Milagro</figcaption>
+      </figure>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760753276/diploma1_b7zo2o.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760753276/diploma1_b7zo2o.jpg" alt="Diplomas – Foto 4" loading="lazy">
+        <figcaption class="overlay">Coon unos de mis compañeros con nuestros diplomas y medallas por mejor STAND</figcaption>
+      </figure>
+    </div>
+
+    <div class="grid grid--2" style="margin-top:16px">
+      <div class="video-wrap"><video src="https://res.cloudinary.com/dqubpavb8/video/upload/v1760756158/20251017_121815_bkram8.mp4" controls playsinline></video></div>
+      <div class="video-wrap"><video src="https://res.cloudinary.com/dqubpavb8/video/upload/v1760755726/diploma1_igtxrx.mp4" controls playsinline></video></div>
     </div>
   </section>
 
 
   <!-- TORNEO DE JUEGOS-->
   <section class="section-card" data-anim>
-    <h2 class="section-title">Torneo de jeugos · 1:00 PM</h2>
-    <div class="meta">Auditorio 1 Equipos de 4 personas</div>
-    <p class="desc">Entre las propuestas destacadas, un equipo presentó una plataforma web para agendar citas veterinarias, enfocada en mejorar la atención y el bienestar animal; mientras que otro grupo desarrolló un sistema de gestión de eventos, ponencias y asistencias, pensado para optimizar la organización académica y tecnológica.</p>
+    <h2 class="section-title">Torneo de Videojuegos Clash Royal · 1:00 PM</h2>
+    <div class="meta">Computo A </div>
+    <p class="desc">El Torneo de Videojuegos de Clash Royale, realizado en el laboratorio “Compuro A” finalizando asi la penultima actividad de la Semana de Sistemas 2025, <br> 
+                    fue una de las actividades más emocionantes del evento. Los participantes compitieron en intensas batallas uno contra uno, demostrando su estrategia, reflejos y dominio<br>
+                    En este torneo habia dos grupos el de los que hiban ganando y el de los perdores que tenian una segunda oportunidad depesue de perder su primer partida.<br>
+                    En este torneo quedo como campeon el compañero Mike. Para ver las fotos completas dar clic en ellas <strong>#SDS25</strong>.</p>
     <div class="grid grid--5" style="margin-top:16px">
-      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760594899/hack1_oq1ooa.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760594899/hack1_oq1ooa.jpg" alt="Inauguración – Foto 1" loading="lazy">
-        <figcaption class="overlay">Hackathon</figcaption>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760756063/torneo3_vgdzbr.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760756063/torneo3_vgdzbr.jpg" alt="Torneo – Foto 1" loading="lazy">
+        <figcaption class="overlay">Torneo de Videojuegos Clash Royale</figcaption>
       </figure>
-      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760594900/hack2_gdxv9i.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760594900/hack2_gdxv9i.jpg" alt="Inauguración – Foto 1" loading="lazy">
-        <figcaption class="overlay">Hackathon</figcaption>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760756060/torneo1_zum2f5.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760594900/hack2_gdxv9i.jpg" alt="Torneo – Foto 2" loading="lazy">
+        <figcaption class="overlay">Torneo de Videojuegos Clash Royale</figcaption>
       </figure>
-      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760594899/hack3_jcmuho.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760594899/hack3_jcmuho.jpg" alt="Inauguración – Foto 1" loading="lazy">
-        <figcaption class="overlay">Hackathon</figcaption>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760756059/torneo5_l34thq.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760756059/torneo5_l34thq.jpg" alt="Torneo – Foto 3" loading="lazy">
+        <figcaption class="overlay">Torneo de Videojuegos Clash Royale</figcaption>
       </figure>
-      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760594903/hack4_nsyseo.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760594903/hack4_nsyseo.jpg" alt="Inauguración – Foto 1" loading="lazy">
-        <figcaption class="overlay">Hackathonl</figcaption>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760756064/torneo2_y2j80l.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760756064/torneo2_y2j80l.jpg" alt="Torneo – Foto 4" loading="lazy">
+        <figcaption class="overlay">Torneo de Videojuegos Clash Royale</figcaption>
       </figure>
-      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760594904/hack5_grer8m.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760594904/hack5_grer8m.jpg" alt="Inauguración – Foto 1" loading="lazy">
-        <figcaption class="overlay">Hackathon</figcaption>
+      <figure class="card" data-full="https://res.cloudinary.com/dqubpavb8/image/upload/v1760756054/torneo4_ugpm4c.jpg"><img src="https://res.cloudinary.com/dqubpavb8/image/upload/v1760756054/torneo4_ugpm4c.jpg" alt="Torneo – Foto 5" loading="lazy">
+        <figcaption class="overlay">Torneo de Videojuegos Clash Royale</figcaption>
       </figure>
-    </div>
-
-    <div class="grid grid--2" style="margin-top:16px">
-      <div class="video-wrap"><video src="https://res.cloudinary.com/dqubpavb8/video/upload/v1760595040/hackk_bbbx2o.mp4" controls playsinline></video></div>
     </div>
   </section>
 
